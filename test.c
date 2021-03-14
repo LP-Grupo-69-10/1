21fb13aa8e30e8264bf1f1e48e91f99f084c64fa
thetruezau <80555837+thetruezau@users.noreply.github.com>
2021-03-14 23:19:11 +0000

    Update test.c

--- test.c
+++ test.c
@@ -5,21 +5,26 @@
 int main() {
   list l = new_list();
   int n;
-  scanf("%d", &n);
+  scanf("%d",&n);
 
-  for(int i = 0; i < n; i++) {
+  for(int i=0; i<n; i++) {
     byte a;
-    scanf("%c", &a);
+    scanf("%hhd",&a);
     task *t = new_task();
-    set_priority(t, a);
-    insert(l, t);
+    set_priority(t,a);
+    set_id(t,(unsigned short)i+1);    
+    insert(l,t);
   }
 
-  list run = l;
+  list _1,_2;
+  search_id(l,3,&_1,&_2);
+  remove_task(l,_2->data);
+
+  list run = l->next;
   while(run != NULL) {
-    printf("%c\n", run->data->priority);
+    printf("%hhd\n", run->data->priority);
     run = run->next;
   }
-  
+
   return 0;
 }
