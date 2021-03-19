30203718e6eea5d5f473fa2f13df0d0ff89d46a9
MooDoesCow <71234181+MooDoesCow@users.noreply.github.com>
2021-03-19 20:05:05 +0000

    Add files via upload

--- libs/list.c
+++ libs/list.c
@@ -1,10 +1,26 @@
+#include <stdio.h>
+#include <time.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdio.h>
 #include "list.h"
+#include "task.h"
 
 list new_list() {
-  return (list)malloc(sizeof(node));
+  list l = (node*)malloc(sizeof(node));
+  l->data = NULL;
+  l->next = NULL;
+  return l;
+}
+
+void add_first(list l, task *t) {
+  list to_add = (list)malloc(sizeof(node));
+  list post = l->next;
+
+  if(to_add != NULL) {
+    to_add->data = t;
+    l->next = to_add;
+    to_add->next = post;
+  }
 }
 
 void search_priority(list l, byte key, list *prev, list *cur) {
@@ -72,14 +88,16 @@ list person_list(list l, char *person) {
 void print_list(list l, byte b) {
   while((l = l->next) != NULL) {
 
-    if(b &  1) printf("%hu\n", l->data->id);
-    if(b &  2) printf("%hhd\n", l->data->priority);
-    if(b &  4) printf("time\n");
-    if(b &  8) printf("%s\n", l->data->description);
-    if(b & 16) printf("%s\n", l->data->person);
-    if(b & 32) printf("time max\n");
-    if(b & 64) printf("time cls\n");
+    if( b &  1) printf("id: %hu\n", l->data->id);
+    if( b &  2) printf("prioridade: %hhd\n", l->data->priority);
+    if( b &  4) printf("criado em %s", ctime(&l->data->creation));
+    if( b &  8) printf("descricao: %s\n", l->data->description);
+    if( b & 16) printf("responsavel: %s\n", l->data->person);
+    if((b & 32) && l->data->deadline != 0)
+      printf("prazo a %s", ctime(&l->data->deadline));
+    if((b & 64) && l->data->conclusion != 0)
+      printf("concluido a %s", ctime(&l->data->conclusion));
     
-    printf("\n");
+    putchar('\n');
   }
 }
---
+++ libs/safe.c
@@ -0,0 +1,119 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "safe.h"
+
+void read_safe_num(int *a, char* msg) {
+  printf("%s", msg);
+  
+  char *buf = malloc(100 * sizeof(char));
+  char *t_buf = malloc(100 * sizeof(char));
+  
+  fgets(buf, 60, stdin);
+  *(buf+strlen(buf)-1) = '\0';
+  
+  *a = atoi(buf);
+  sprintf(t_buf, "%d", *a);
+  
+  if(strcmp(t_buf, buf) != 0) { 
+    wrong_input();
+    read_safe_num(a, msg);
+  }
+  
+  free(buf);
+  free(t_buf);
+}
+
+void read_safe_int(int* a, char* msg) {
+  int t;
+  read_safe_num(&t, msg); 
+  *a = t;
+}
+
+void read_safe_byte(byte *a, char* msg) {
+  int t;
+  read_safe_num(&t, msg);
+  *a = (byte)t;
+  if((int)*a != t) {
+    wrong_input();
+    read_safe_byte(a, msg);
+  }
+}
+
+void read_safe_ushort(unsigned short *a, char *msg) {
+  int t;
+  read_safe_num(&t, msg);
+  *a = (unsigned short)t;
+  if((int)*a != t) read_safe_ushort(a, msg);
+}
+
+void read_safe_string(char *a, char *msg) {
+  char *buf = malloc(60 * sizeof(char));
+  
+  if(buf == NULL) printf("Sem memória disponível. Devias começar a trabalhar!");
+  else {
+    printf("%s", msg);
+    fgets(buf, 60, stdin);
+    strcpy(a, buf);
+  }
+  free(buf);
+}
+
+void read_safe_date(time_t *a, char *msg) {
+  printf("%s", msg);
+  int min, hour, day, month, year;
+  
+  read_safe_int(&year,  "Insira o ano: ");
+  read_safe_int(&month, "Insira o mês: ");
+  read_safe_int(&day,   "Insira o dia: ");
+  read_safe_int(&hour,  "Insira a hora: ");
+  read_safe_int(&min,   "Insira o minuto: ");
+  // mudar estrutura;
+  
+  if(1/*NOT NULL a*/) {
+    wrong_input();
+    read_safe_date(a, msg);
+  }
+}
+
+// -------------------------------------------------------------------------------- //
+
+void read_option(byte *a) {
+  read_safe_byte(a, "\nInsira uma opção: ");
+  if(*a == 0 || *a > 10) {
+    wrong_input();
+    read_option(a);
+  }
+}
+
+void read_id(unsigned short *a) {
+  read_safe_ushort(a, "\nInsira o ID da tarefa: ");
+  if(1/*ID DOESNT EXIT*/) {
+    wrong_input();
+    read_id(a);
+  }
+}
+
+void read_priority(byte *a) {
+  read_safe_byte(a, "Insira a prioridade da tarefa (1-10): ");
+  if(*a > 10) {
+    wrong_input();
+    read_priority(a);
+  }
+}
+
+void read_description(char *a) {
+  read_safe_string(a, "Insira a descrição desta tarefa: ");
+}
+
+void read_person(char *a) {
+  read_safe_string(a, "Insira o nome do responsável por esta tarefa: ");
+}
+
+void read_deadline(time_t *a) {
+  read_safe_date(a, "Insira a data limite para a conclusão da tarefa:\n");
+}
+
+void wrong_input() {
+  printf("Input Errado!\n");
+}
