ed5b64332738e3dc2da1f286fc4701786f30cfb7
thetruezau <80555837+thetruezau@users.noreply.github.com>
2021-03-19 22:11:56 +0000

    Add files via upload

--- test.c
+++ test.c
@@ -1,12 +1,17 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
 #include <string.h>
 #include "libs/list.h"
+#include "libs/task.h"
+#include "libs/file.h"
 
-int main() {
-  int n;
+void testar1() {
+  int n;  
   scanf("%d", &n);
   list l = new_list();
+
+  load_id("to_do");
   
   for(int i = 0; i < n; i++) {
     task *t = new_task();    
@@ -14,17 +19,27 @@ int main() {
     scanf("%hhd", &a);
 
     set_priority(t, a);
-    set_id(t, (unsigned short)i+1);
-    set_person(t, "michael");
     set_description(t, "lavar loiça");
-
+    set_person(t, "michael");
+    
     insert(l,t);
   }
+  
+  print_list(l, 127);  
+  write_lf(l, "to_do");  
+}
 
-  // edit_person(l, 3, "ola");
-  // list lp = person_list(l, "oi");
+void testar2() {
+  puts("---");
+  
+  list nl = read_fl("to_do");
+  print_list(nl, 127);
 
-  print_list(l, 127);
+}
 
+int main() {
+  // testar1();
+  testar2();
+  
   return 0;
 }
