b1c526beed7da886b89dcbb8d4498c766fc6acae
MooDoesCow <71234181+MooDoesCow@users.noreply.github.com>
2021-03-22 16:21:05 +0000

    Add files via upload

--- main.c
+++ main.c
@@ -7,52 +7,11 @@
 #include "libs/file.h"
 #include "libs/safe.h"
 
-/*
-void testar1() {
-  list l = new_list();
-  time_t timer = time(NULL);
-  
-  printf("---\n%s---\n", ctime(&timer));
-
-  int n;
-  scanf("%d", &n);
-  
-  for(int i = 0; i < n; i++) {
-    task *t = new_task();    
-    byte a;
-    scanf("%hhd", &a);
-
-    set_id(t, (unsigned short)i+1);
-    set_priority(t, a);
-    set_description(t, "lavar loiça");
-    set_person(t, "michael");
-    
-    insert(l,t);
-  }
-  
-  print_list(l, 127);
-  
-  write_lf(l, "testfile");
-
-  list nl = read_fl("testfile");
-  puts("--- --- --- ---");
-  print_list(nl, 127);
-  
-}
-*/
-
-/*
-void testar2() {
-  list l = read_fl("testfile");
-  print_list(l, 127);
-}
-*/
-
 int main() {
   printf("+----------------+\n");
   printf("|Quadro de KanBan|\n");
   printf("+----------------+\n");
-
+  
   int exit_status = 0;
   byte option;
   
