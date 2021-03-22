3152e03209dd073154514003342897e77765ebfd
Sofia Teixeira <80487252+sofiateixeira2@users.noreply.github.com>
2021-03-22 15:34:34 +0000

    Add files via upload

--- test.c
+++ test.c
@@ -1,45 +1,123 @@
-#include <stdio.h>
-#include <time.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <string.h>
 #include "libs/list.h"
-#include "libs/task.h"
-#include "libs/file.h"
 
-void testar1() {
-  int n;  
-  scanf("%d", &n);
-  list l = new_list();
+void menu() {
+  printf("\n----------------------------------------------------\n");
+  printf("MENU:\n\n");
+  printf("1 - Inserir nova tarefa\n");
+  printf("2 - Mover cartões\n");
+  printf("3 - Alterar a pessoa responsável\n");
+  printf("4 - Fechar tarefa\n");
+  printf("5 - Reabrir tarefa\n");
+  printf("6 - Visualizar o quadro\n");
+  printf("7 - Visualizar as tarefas de uma pessoa\n");
+  printf("8 - Visualizar tarefas ordenadas por data de criação\n");
+  printf("0 - Terminar\n");
+  printf("----------------------------------------------------\n");
 
-  load_id("to_do");
-  
-  for(int i = 0; i < n; i++) {
-    task *t = new_task();    
-    byte a;
-    scanf("%hhd", &a);
+  printf("\n-> ");
 
-    set_priority(t, a);
-    set_description(t, "lavar loiça");
-    set_person(t, "michael");
-    
-    insert(l,t);
-  }
-  
-  print_list(l, 127);  
-  write_lf(l, "to_do");  
 }
 
-void testar2() {
-  puts("---");
-  
-  list nl = read_fl("to_do");
-  print_list(nl, 127);
+void check_opt(list l, int opt) {
 
+  int a;
+  switch (opt) {
+    case 1: ;//inserir nova tarefa
+      task *t = new_task();
+      int tmpid = 1, tmppt = 10;
+      //printf("*Insert id*\n\n-> "); //temporary
+      //unsigned short id;
+      //scanf("%hu", &id);
+      set_id(t, tmpid++);
+      //printf("*Insert priority*\n\n-> "); //temporary
+      //byte priority;
+      //scanf("%c", &priority);
+      set_priority(t, tmppt--);
+      //set_description();
+      insert(l, t);
+      printf("\nDeseja mais alguma opção?\n");
+      printf("(1 - yes, 0 - no)\n\n-> ");
+      scanf("%d", &a);
+      //printf("xcfhcvgj\n");
+      if(a == 1) {
+        menu();
+        int i;
+        scanf("%d", &i);
+        check_opt(l, i);
+      } else break;
+      break;
+    case 2: //mover cartões
+      break;
+    case 3: //alterar pessoa responsável
+      //edit_person();
+      break;
+    case 4: //fechar tarefa
+      break;
+    case 5: //reabrir tarefa
+      break;
+    case 6: //visualizar quadro
+      break;
+    case 7: ;//visualizar todas as tarefas de uma pessoa
+      char *s = "H";
+      list lp = person_list(l, s);
+      print_list(lp, 127);
+      printf("\nDeseja mais alguma opção?\n");
+      printf("(1 - yes, 0 - no)\n\n-> ");
+      scanf("%d", &a);
+      //printf("xcfhcvgj\n");
+      if(a == 1) {
+        menu();
+        int i;
+        scanf("%d", &i);
+        check_opt(l, i);
+      } else break;
+      break;
+    case 8: //visualizar tarefas ordenadas por ordem de data de criação
+      break;
+    case 0: //termina
+      break;
+  }
 }
 
 int main() {
-  // testar1();
-  testar2();
-  
+
+  menu();
+  int i;
+  scanf("%d", &i);
+  list l = new_list();
+  check_opt(l, i);
+  int n;
+  scanf("%d",&n);
+
+  /*for(int i=0; i<5; i++) {
+    byte a;
+    scanf("%hhd",&a);
+    task *t = new_task();
+    set_priority(t,a);
+    set_id(t,(unsigned short)i+1);
+    char *s = "oi";
+    set_person(t, s);
+    set_description(t, "lavar loiça");
+    insert(l,t);
+  }*/
+
+  /*list _1,_2;
+  search_id(l,3,&_1,&_2);
+  remove_task(l,_2->data);*/
+
+  //edit_person(l, 3, "H");
+
+  //list lp = person_list(l, "oi");
+  //print_list(lp, 127);
+
+  /*list run = l->next;
+  while(run != NULL) {
+    printf("%hhd %s\n", run->data->priority, run->data->person);
+    run = run->next;
+  }*/
+
   return 0;
 }
