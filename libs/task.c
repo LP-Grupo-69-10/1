e6b6139ec326cc5cd251b94cdaa46da203813ff8
thetruezau <80555837+thetruezau@users.noreply.github.com>
2021-03-22 15:41:59 +0000

    Add files via upload

--- libs/task.c
+++ libs/task.c
@@ -1,31 +1,36 @@
 #include <time.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "list.h"
 #include "task.h"
+
+unsigned short ID_COUNT = 1;
 
 task *new_task() {
   task *t = (task*)malloc(sizeof(task));
-  t->id = 0;
+  t->id = ID_COUNT++;
   t->priority = 0;
   t->creation = time(NULL);
-  t->description[0] = 0;
-  t->person[0] = 0;
+  t->description[0] = '\0';
+  t->person[0] = '\0';
   t->deadline = (time_t)0;
   t->conclusion = (time_t)0;
     
   return t;
 }
 
-void set_id(task *t, unsigned short i) {
-  t->id = i;
+void load_id(char *filename) {
+  FILE *fp = fopen(filename, "rb");
+
+  if(fp != NULL)
+    (void)fread(&ID_COUNT, sizeof(unsigned short), 1, fp);
+
+  fclose(fp);
 }
 
 void set_priority(task *t, byte p) {
   t->priority = p;
-}
-
-void set_creation(task *t, time_t d) {
-  t->creation = d;
 }
 
 void set_description(task *t, char *d) {
