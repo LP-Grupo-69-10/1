52b1d8b59b4555cb2c9302388080e5fa748e3064
MooDoesCow <71234181+MooDoesCow@users.noreply.github.com>
2021-03-24 01:55:15 +0000

    Add files via upload

--- main.c
+++ main.c
@@ -6,31 +6,37 @@
 #include "libs/task.h"
 #include "libs/file.h"
 #include "libs/safe.h"
+#include "libs/menu.h"
 
 int main() {
   printf("+----------------+\n");
   printf("|Quadro de KanBan|\n");
   printf("+----------------+\n");
   
+  load_lists();
+  
   int exit_status = 0;
   byte option;
   
   while(!exit_status) {
-    //print_menu();
+    print_menu();
     read_option(&option);
     
     switch(option) {
-    case 1: /* insert_new_task(); */ break;
-    case 2: /* start_task(); */ break;
-    case 3: /* change_responsible(); */ break;
-    case 4: /* end_task(); */ break;
-    case 5: /* redo_task(); */ break;
-    case 6: /* print_board(); */ break;
-    case 7: /* person_list(); */ break;
-    case 8: /* print_by_creation(); */ break;
-    case 9: exit_status = 1;
+    case 1: insert_new_task(); break;
+    case 2: start_task(); break;
+    case 3: change_responsible(); break;
+    case 4: end_task(); break;
+    case 5: redo_task(); break;
+    case 6: print_board(); break;
+    case 7: print_by_person(); break;
+    case 8: print_by_creation(); break;
+    case 0: exit_status = 1;
     }
+    
+    if(option <= 4)
+      write_lists();
   }
-  
+
   return EXIT_SUCCESS;
 }
