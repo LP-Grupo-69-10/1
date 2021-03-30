f018a09204bb0b24ea47d50fd35dbbd6d92e9585
MooDoesCow <71234181+MooDoesCow@users.noreply.github.com>
2021-03-30 23:12:38 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -15,9 +15,9 @@
 #include "libs/menu.h"
 
 int main() {
-  printf("+----------------+\n");
-  printf("|Quadro de KanBan|\n");
-  printf("+----------------+\n");
+  printf("+------------------+\n");
+  printf("| Quadro de KanBan |\n");
+  printf("+------------------+\n");
   
   load_lists();
   
@@ -30,13 +30,14 @@ int main() {
     
     switch(option) {
     case 1: insert_new_task(); break;
-    case 2: start_task(); break;
-    case 3: change_responsible(); break;
-    case 4: end_task(); break;
-    case 5: redo_task(); break;
-    case 6: print_board(); break;
-    case 7: print_by_person(); break;
-    case 8: print_by_creation(); break;
+    case 2: see_task(); break;
+    case 3: start_task(); break;
+    case 4: change_responsible(); break;
+    case 5: end_task(); break;
+    case 6: redo_task(); break;
+    case 7: print_by_priority(); break;
+    case 8: print_by_person(); break;
+    case 9: print_by_creation(); break;
     case 0: exit_status = 1;
     }
     
