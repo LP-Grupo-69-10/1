57d11f21d3bbe9bf30cb57124f31303a1305ed59
thetruezau <80555837+thetruezau@users.noreply.github.com>
2021-04-03 00:18:52 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -28,22 +28,22 @@ int main() {
     print_menu();
     read_option(&option);
 
-    clean_screen();
+    clear_screen();
     
     switch(option) {
-    case 1: insert_new_task(); break;
-    case 2: see_task(); break;
-    case 3: start_task(); break;
-    case 4: change_responsible(); break;
-    case 5: end_task(); break;
-    case 6: redo_task(); break;
-    case 7: print_by_priority(); break;
-    case 8: print_by_person(); break;
-    case 9: print_by_creation(); break;
+    case 1: insert_new_task();    clear_screen(); break;
+    case 2: see_task();                           break;
+    case 3: start_task();         clear_screen(); break;
+    case 4: change_responsible(); clear_screen(); break;
+    case 5: end_task();           clear_screen(); break;
+    case 6: redo_task();          clear_screen(); break;
+    case 7: print_by_priority();                  break;
+    case 8: print_by_person();                    break;
+    case 9: print_by_creation();                  break;
     case 0: exit_status = 1;
     }
     
-    if(option <= 4)
+    if(option <= 6)
       write_lists();
   }
   
