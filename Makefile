304698d5ab6fcf810304a875be2c86336b620132
thetruezau <80555837+thetruezau@users.noreply.github.com>
2021-04-08 22:28:41 +0100

    Add files via upload

--- Makefile
+++ Makefile
@@ -1,11 +1,11 @@
 compile:
-	gcc -Wall libs/*.c main.c -o .main
+	gcc -Wall libs/*.c main.c -o kanban
 
 run:
-	./.main
+	./kanban
+
+clear:
+	rm -rf files/*
 
 emacs:
 	emacs main.c libs/* &
-
-clear:
-	rm -rf .files/*
--- main.c
+++ main.c
@@ -15,6 +15,7 @@
 #include "libs/menu.h"
 
 int main() {
+  clear_screen();
   printf("+------------------+\n");
   printf("| Quadro de KanBan |\n");
   printf("+------------------+\n");
@@ -31,15 +32,15 @@ int main() {
     clear_screen();
     
     switch(option) {
-    case 1: insert_new_task();    clear_screen(); break;
-    case 2: see_task();                           break;
-    case 3: start_task();         clear_screen(); break;
-    case 4: change_responsible(); clear_screen(); break;
-    case 5: end_task();           clear_screen(); break;
-    case 6: redo_task();          clear_screen(); break;
-    case 7: print_by_priority();                  break;
-    case 8: print_by_person();                    break;
-    case 9: print_by_creation();                  break;
+    case 1: insert_new_task();    break;
+    case 2: see_task();           break;
+    case 3: start_task();         break;
+    case 4: change_responsible(); break;
+    case 5: end_task();           break;
+    case 6: redo_task();          break;
+    case 7: print_by_priority();  break;
+    case 8: print_by_person();    break;
+    case 9: print_by_creation();  break;
     case 0: exit_status = 1;
     }
     
